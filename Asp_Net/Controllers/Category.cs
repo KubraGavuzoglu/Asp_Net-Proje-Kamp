b76e3b558ad7791a1a618db7ad1516a53d008de1
KubraGavuzoglu <100765765+KubraGavuzoglu@users.noreply.github.com>
2022-12-31 14:24:01 +0300

    Add project files.

---
+++ Asp_Net/Controllers/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp_Net.Controllers
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
